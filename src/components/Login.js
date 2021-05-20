99ff060fc9e5d4087f03b6cfa3acc18b7cf29b3c
notwarnite <180010028@iitdh.ac.in>
2021-05-20 23:15:32 +0530

    react-js_clone

---
+++ src/components/Login.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Login() {
+    return (
+        <Container>
+            <CTA>
+                <CTALogoOne src="/images/cta-logo-one.svg" />
+                <SignUp>Enter The World</SignUp>
+                <Description>
+                    Get premier access to all your favourite services!!!
+                </Description>
+                <CTALogoTwo src="/images/cta-logo-two.png" />
+            </CTA>
+        </Container>
+    )
+}
+
+export default Login
+
+const Container = styled.div`
+
+    position: relative;
+    height: calc(100vh - 70px);
+    display: flex;
+    align-items: top;
+    justify-content: center;
+
+    &:before {
+        position: absolute;
+        content: "";
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-position: top;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-image: url("/images/login-background.jpg");
+        opacity: 0.7;
+        z-index: -1;
+
+    }
+
+`
+
+
+const CTA = styled.div`
+    max-width: 650px;
+    padding: 80px 40px;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    margin-top: 150px;
+    align-items: center;
+
+`
+
+const CTALogoOne = styled.img`
+
+`
+
+const CTALogoTwo = styled.img`
+    width: 90%;
+`
+
+
+const SignUp = styled.a`
+    width: 100%;
+    background-color: #0063e5;
+    font-weight: bold;
+    padding: 17px 0;
+    color: #f9f9f9;
+    border-radius: 4px;
+    text-align: center;
+    font-size: 18px;
+    cursor: pointer;
+    transition: all 250ms;
+    letter-spacing: 1.5px;
+    margin-top: 8px;
+    margin-bottom: 12px;
+
+    &:hover {
+        background: #0483ee;
+    }
+`
+
+const Description = styled.div`
+    font-size: 11px;
+    letter-spacing: 1.5px;
+    text-align: center;
+    line-height: 1.5;
+
+`
